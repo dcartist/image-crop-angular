3d53ad3d9c56d39aada3bfc2a61d6dcb65aa90b7
dcartist <pbannerman13@gmail.com>
2022-04-21 16:23:57 -0400

    updating files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { ImageCroppedEvent } from 'ngx-image-cropper';
+// import { Dimensions, ImageCroppedEvent, ImageTransform } from './image-cropper/interfaces/index';
+import {base64ToFile} from './image-cropper/utils/blob.utils';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'image-crop-angular';
+  imgChangeEvt: any = '';
+    cropImgPreview: any = '';
+    onFileChange(event: any): void {
+        this.imgChangeEvt = event;
+    }
+    cropImg(e: ImageCroppedEvent) {
+        this.cropImgPreview = e.base64;
+    }
+    imgLoad() {
+        // display cropper tool
+    }
+    initCropper() {
+        // init cropper
+    }
+    
+    imgFailed() {
+        // error msg
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ImageCropperModule } from 'ngx-image-cropper';
+// import { ImageCropperModule } from './image-cropper/image-cropper.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent 
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule, ImageCropperModule,
     AppRoutingModule
   ],
   providers: [],
